5d12fc887038369c0867f0d3be90341c9b41c964
Omar El Hajj <omar.elhajj.00@gmail.com>
2022-09-03 22:42:20 +0300

    made the mouse enter listener

--- Maze Js Game/game.js
+++ Maze Js Game/game.js
@@ -1,37 +1,78 @@
 //execute when the window finish , wait the window to load
 window.onload = function () {
-    var mousePosition;
-    // var offset = [0,0];
-    var isDown = false;
-    var m = document.getElementById('start');
 
-    // document.body.appendChild(m);
 
-    m.addEventListener('mousedown', function(e) {
-        isDown = true;
-        offset = [
-            m.offsetLeft - e.clientX,
-            m.offsetTop - e.clientY
-        ];
-    }, true);
 
-    document.addEventListener('mouseup', function() {
-        isDown = false;
-    }, true);
+    var game = document.getElementById("game");
+    var start = document.getElementById("start");
+    var score = 0;
+    var boundaries = document.getElementsByClassName("boundary");
+    startButton = document.getElementsByClassName('boundary')[5];
+    b = Array.from(boundaries);
+    var lost = false;
 
-    document.addEventListener('mousemove', function(event) {
-        event.preventDefault();
-        if (isDown) {
-            mousePosition = {
+    start.addEventListener("mouseenter", enterTheNarrowArea);
+    
 
-                x : event.clientX,
-                y : event.clientY
-
-            };
-            m.style.left = (mousePosition.x + offset[0]) + 'px';
-            m.style.top  = (mousePosition.y + offset[1]) + 'px';
+    function enterTheNarrowArea() {
+        for (var i = 0; i < boundaries.length-1;i++){
+            boundaries[i].addEventListener("mouseover", gameOver);
         }
-    }, true);
+    }
+
+    function gameOver() {
+        for (var i = 0; i < boundaries.length-1;i++){
+            boundaries[i].style.backgroundColor = "red";
+        }
+        startButton.innerHTML = "Reset";
+        startButton.style.padding = "0rem auto";
+        
+    }
+    function reset() {
+        
+    }
+
+
+    // function gameOver() {
+    //     document.write('<form style><h3>Game Over</h3><form>')
+    // }
+    // // var mousePosition;
+    // // var offset = [0,0];
+    // var isDown = false;
+    // var m = document.getElementById('start');
+
+    // // document.body.appendChild(m);
+
+    // m.addEventListener('mousedown', function(e) {
+    //     isDown = true;
+    //     offset = [
+    //         m.offsetLeft - e.clientX,
+    //         m.offsetTop - e.clientY
+    //     ];
+    // }, true);
+
+    // document.addEventListener('mouseup', function() {
+    //     isDown = false;
+    // }, true);
+
+    // document.addEventListener('mousemove', function(event) {
+    //     event.preventDefault();
+    //     if (isDown) {
+    //         mousePosition = {
+
+    //             x : event.clientX,
+    //             y : event.clientY
+
+    //         };
+    //         m.style.left = (mousePosition.x + offset[0]) + 'px';
+    //         m.style.top  = (mousePosition.y + offset[1]) + 'px';
+    //     }
+    // }, true);
+
+
+
+
+
     // var m = document.getElementById('start');
     // m.addEventListener('mousedown', mouseDown, false);
     // window.addEventListener('mouseup', mouseUp, false);
